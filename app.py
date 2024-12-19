801af0dbbeb7586da410cd2a437b2f809bf942dd
Vu <anhvuleduc@gmail.com>
2024-12-19 18:28:51 +0700

    Add Greedy Search & Toggle Function

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ from shortest_path_algorithms import *
 
 app = Flask(__name__)
 
-
 # Unzip the graphml file if neccessary
 if os.path.exists('./data/graph.graphml') == False: 
     shutil.unpack_archive('data.zip', '.')
-    
+
 G = ox.load_graphml('./data/graph.graphml')
 
 # Ensure future commits don't have large file
@@ -19,14 +18,16 @@ os.remove('./data/graph.graphml')
 os.removedirs('./data')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    node_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in G.nodes]
+    return render_template('index.html', node_coords=node_coords)
 
 algorithm_list = {
     'Dijkstra': Dijkstra, 
     'DFS': DFS, 
     'BFS': BFS, 
     'A Star': A_Star, 
-    'Depth Limited DFS': depth_limited_DFS
+    'Depth Limited DFS': DLS,
+    'Greedy Search': GFS
 }
 @app.route('/find_shortest_path', methods=['POST'])
 def find_shortest_path():
@@ -37,7 +38,9 @@ def find_shortest_path():
     max_depth = int(data['max_depth'])
     # Find the nearest nodes on the graph to the clicked points
     start_node = ox.distance.nearest_nodes(G, start_coords[1], start_coords[0])  # lon, lat
+    print(start_coords)
     end_node = ox.distance.nearest_nodes(G, end_coords[1], end_coords[0])
+    print(end_coords)
     
     func = algorithm_list.get(algorithm)
     if not func:
@@ -45,7 +48,7 @@ def find_shortest_path():
 
     # Calculate the path using the selected algorithm
     path_coords = []
-    if func != depth_limited_DFS:
+    if func != DLS:
         path_coords = func(G, start_node, end_node)
     else:
         path_coords = func(G, start_node, end_node, max_depth)
--- shortest_path_algorithms.py
+++ shortest_path_algorithms.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from queue import PriorityQueue
 def DFS(G, start_node, end_node):
     dfs_edges = list(nx.dfs_edges(G, source=start_node))
     path = []
@@ -8,7 +9,6 @@ def DFS(G, start_node, end_node):
         path.append(node)
         node = parent[node]
     path.append(start_node)
-    
     # Get the coordinates for the path
     path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in path]
     return path_coords
@@ -22,7 +22,7 @@ def BFS(G, start_node, end_node):
         path.append(node)
         node = parent[node]
     path.append(start_node)
-    
+
     # Get the coordinates for the path
     path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in path]
     return path_coords
@@ -36,27 +36,62 @@ def Dijkstra(G, start_node, end_node):
 
 def A_Star(G, start_node, end_node):
     shortest_path = nx.astar_path(G, start_node, end_node, weight='length')
-
+    #shortest_path.append(start_node)
     # Get the coordinates for the shortest path
     path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in shortest_path]
     return path_coords
 
-def depth_limited_DFS(G, start_node, end_node, max_depth):
-    def recursive_dls(node, depth):
-        if node == end_node:
-            return [node]
-        if depth == 0:
-            return None
-        for neighbor in G.neighbors(node):
-            path = recursive_dls(neighbor, depth - 1)
-            if path:
-                return [node] + path
+def DLS(G, start_node, end_node, max_depth):
+    dfs_edges = list(nx.dfs_edges(G, source=start_node, depth_limit=max_depth))
+    path = []
+    parent = {v:u for u, v in dfs_edges}
+    if parent.get(end_node) is None:
         return None
-
-    path = recursive_dls(start_node, max_depth)
-    if path is None:
-        return None
+    node = end_node
+    while node != start_node:
+        path.append(node)
+        node = parent[node]
+    path.append(start_node)
 
     # Get the coordinates for the path
+    path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in path]
+    return path_coords
+
+def GFS(G, start_node, end_node):
+    def heuristic(G, node, goal):
+        x1, y1 = G.nodes[node]['x'], G.nodes[node]['y']
+        x2, y2 = G.nodes[goal]['x'], G.nodes[goal]['y']
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    
+    visited = set()
+    q = PriorityQueue()
+    q.put((0, start_node))
+    parent = {}
+
+    while not q.empty():
+        _, current_node = q.get()
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+
+        if current_node == end_node:
+            break
+
+        for neighbor in G.neighbors(current_node):
+            if neighbor not in visited:
+                priority = heuristic(G, neighbor, end_node)
+                q.put((priority, neighbor))
+                parent[neighbor] = current_node
+
+    if end_node not in parent:
+        return None
+
+    path = []
+    node = end_node
+    while node != start_node:
+        path.append(node)
+        node = parent[node]
+    path.append(start_node)
+    #path.reverse()
     path_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in path]
     return path_coords
