73a44d5cc58650c98728cebb2478c7e243b17f4d
already-ded <ngocsctn0401@gmail.com>
2025-05-13 14:26:12 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import osmnx as ox
 import networkx as nx
 import os
 import shutil
+import xml.etree.ElementTree as et
 from shortest_path_algorithms import *
 
 app = Flask(__name__)
@@ -56,6 +57,71 @@ def find_shortest_path():
         return jsonify({"error": "No path found"}), 404
 
     return jsonify({'path_coords': path_coords, 'max_depth': max_depth})
+
+# Đường dẫn tương đối đến file graphml
+graphml_file = os.path.join(os.path.dirname(__file__), 'trungliet.graphml')
+
+# Parse file graphml để lấy danh sách roadname
+tree = et.parse(graphml_file)
+root = tree.getroot()
+
+# Namespace của GraphML
+namespace = {'graphml': 'http://graphml.graphdrawing.org/xmlns'}
+
+# Trích xuất các roadname
+road_names = set()
+for edge in root.findall('.//graphml:edge', namespace):
+    road_name = edge.find('./graphml:data[@key="d11"]', namespace)
+    if road_name is not None and road_name.text:
+        road_names.add(road_name.text)
+
+# Chuyển danh sách roadname thành dạng sắp xếp
+road_names = sorted(road_names)
+
+@app.route('/get_roadnames', methods=['GET'])
+def get_roadnames():
+    # API để gửi danh sách roadname đến frontend
+    return jsonify(road_names)
+
+@app.route('/get_road_geometry', methods=['POST'])
+def get_road_geometry():
+    road_name = request.json.get('road_name')
+    if not road_name:
+        return jsonify({'error': 'Missing road_name'}), 400
+
+    namespace = {'graphml': 'http://graphml.graphdrawing.org/xmlns'}
+    tree = et.parse(graphml_file)
+    root = tree.getroot()
+
+    # Tạo lại key_map như trước (có thể dùng chung nếu đã khai báo toàn cục)
+    key_map = {}
+    for key in root.findall('graphml:key', namespace):
+        key_map[key.attrib.get('id')] = key.attrib.get('attr.name')
+
+    # Tìm các cạnh (edges) có tên khớp với road_name
+    matched_edges = []
+    for edge in root.findall('.//graphml:edge', namespace):
+        edge_name = None
+        for data in edge.findall('graphml:data', namespace):
+            if key_map.get(data.attrib.get('key')) == 'name':
+                edge_name = data.text
+                break
+        if edge_name != road_name:
+            continue
+
+        # Lấy id của 2 node đầu-cuối
+        source = edge.attrib.get('source')
+        target = edge.attrib.get('target')
+
+        # Lấy tọa độ từ graph G
+        try:
+            coord1 = (G.nodes[source]['y'], G.nodes[source]['x'])
+            coord2 = (G.nodes[target]['y'], G.nodes[target]['x'])
+            matched_edges.append([coord1, coord2])
+        except:
+            continue
+
+    return jsonify({'segments': matched_edges})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
